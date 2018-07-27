0002c55747f6ce26803d7b9269214c4dfa525734
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-27 18:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code your solution here
+//
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+//
+function nestedTarget(target) {
+ return document.querySelector(".target")
+}
+//
+function deepestChild(child) {
+  return document.querySelector('#grand-node').innerHTML
+}
